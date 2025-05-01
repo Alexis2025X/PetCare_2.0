909e7348804f4d17effc82f1da3ce3acccb0f2f9
Alexis Castillo <USTR007724@ugb.edu.sv>
2025-05-01 15:55:50 -0600

    Fix: fix errors and windows

--- app/src/main/java/com/alexis/petcare20/Login.java
+++ app/src/main/java/com/alexis/petcare20/Login.java
@@ -3,6 +3,8 @@ package com.alexis.petcare20;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class Login extends AppCompatActivity {
 
     Button btn;
+    ImageButton btnIcon;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +30,30 @@ public class Login extends AppCompatActivity {
         btn.setOnClickListener(view -> {
             abrirPantallaPrincipal();
         });
-
-
+/*        int i = 0;
+        //Para ver o no el password
+        btnIcon = findViewById(R.id.btnVer);
+        btnIcon.setOnClickListener(view -> {
+            boolean click = true;
+        });
+        if(btnIcon.setOnClickListener(view -> {}))
+*//*        btnIcon = findViewById(R.id.btnOcultar);
+        btnIcon.setOnClickListener(view -> {
+            ocultar();
+        });*/
     }
+/*    private void ver() {
+        editText = findViewById(R.id.txtPassword);
+        editText.setInputType(android.text.InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD | android.text.InputType.TYPE_CLASS_TEXT);
+        btnIcon = findViewById(R.id.btnVer);
+        btnIcon.setImageResource(R.drawable.ojo_ver);
+    }
+    private void ocultar() {
+        editText = findViewById(R.id.txtPassword);
+        editText.setInputType(android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD | android.text.InputType.TYPE_CLASS_TEXT);
+        btnIcon = findViewById(R.id.btnVer);
+        btnIcon.setImageResource(R.drawable.ojo_no_ver);
+    }*/
     private void abrirCrearCuenta() {
         // Aquí puedes abrir la ventana de agregar citas
         // Por ejemplo, puedes iniciar una nueva actividad
